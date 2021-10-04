804c88607dfa1f399559b145ecfcbc43de527d71
David Plantiko <david.plantiko@gmail.com>
2021-10-04 18:29:16 +0200

    Equation in code

--- equation/pq_formula.c
+++ equation/pq_formula.c
@@ -6,9 +6,24 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(argc, argv)
+int main(int argc, char *argv[])
 {
+	if(argc<=1)
+	{
+		printf("Too less arguments.\n");
+		return 1;
+	}
+	
 	int x,p,q;
 	
+	p=atoi(argv[1]);
+	q=atoi(argv[2]);
+
+	printf("p=%d\n",p);
+	printf("q=%d\n",q);
+
+	x=((-p)/2)+sqrt((p/2)*(p/2)-q);
+	printf("x=%d\n");
+
 	return 0;
 }
